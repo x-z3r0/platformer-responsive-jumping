0f622da1ffc2040ddb1906c45580950440d09559
PX-41 <toby.konyves@gmail.com>
2020-01-12 21:24:56 +0000

    Added todo and cleaned up.

--- src/index.js
+++ src/index.js
@@ -8,20 +8,20 @@ const player = {
   y: 0,
   yv: 0,
 
-  maxJumpHeight: 400,
-  minJumpHeight: 10,
+  // TODO: limit sensitivity of jumping, i.e. currently to achieve max jump height it requires holding space down until apex.
 
-  maxJumpTime: 20,
+  maxJumpHeight: 250,
+  minJumpHeight: 100,
 
-  jumpKeyLetGo: false,
+  maxJumpTime: 15,
+
+  jumpKeyReleased: false,
 
   isOnGround: false,
   isJumping: false,
 
   width: 75,
   height: 75,
-
-  TEST_framesJumped: 0,
 };
 
 const groundLevel = player.height / -2;
@@ -34,10 +34,10 @@ const handlePhysics = p => {
     if(p.isOnGround) {
       p.yv = initialVelocity;
       p.isJumping = true;
-      p.jumpKeyLetGo = false;
+      p.jumpKeyReleased = false;
     }
-  } else if(p.isJumping && !p.jumpKeyLetGo) {
-    p.jumpKeyLetGo = true;
+  } else if(p.isJumping && !p.jumpKeyReleased) {
+    p.jumpKeyReleased = true;
 
     if(p.yv > 0) {
       let num = gravity * (p.minJumpHeight * 2 - initialVelocity * 2 + gravity);
